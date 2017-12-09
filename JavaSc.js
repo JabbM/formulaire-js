89cd8542188c49a04afb7a25474aca7f484ef3cc
JabbM <33683864+JabbM@users.noreply.github.com>
2017-12-09 12:09:16 +0100

    Add files via upload

--- JavaSc.js
+++ JavaSc.js
@@ -23,58 +23,25 @@ function création(){
 
     if (Form2.style.display = 'none') {
       Form2.style.display = 'block';
-      None.style.display = 'none'
+      None.style.display = 'none';
 
     }
 
 }
-/*
-        }
 
 function enregistrer ()
         {
-          if(document.getElementById('None').style.display = 'block')
+          if(document.getElementById('Form2').style.display = 'block')
           {
-                document.getElementById('None').style.display = 'none';
                 document.getElementById('Form2').style.display = 'none';
                 document.getElementById('fin').style.display = 'block';
 
           }
         }
- function SelectHorraire ()
+ function SelectHoraire ()
         {
-          if (document.getElementById('Horraire').value = "Après diner")
+          if (document.getElementById('Horaire').value = "Après diner")
           {
               document.getElementById('Heure').style.display = "block";
           }
         }
-
-
-  function Ajouter(idclone) {
-
-      var i = 0;
-          clone = document.getElementById('Song').cloneNode(true); //Je clone #Song
-          copy = [];
-          copy =  document.Song.Artiste.value; //je copie ce qu'il y a dans value de #Artiste
-          copy2 = [];
-          copy2 = document.Song.Titre.value;// "   "  "   "  "  " #Titre
-          cloneArtiste = "Artiste" + i;
-          cloneTitre = "Titre" + i;
-          nameclone = "name" + i;
-          idclone = "copie" + i;
-      clone.Artiste.name = cloneArtiste;
-      clone.id = idclone;
-      clone.name = nameclone;
-      clone.style = "margin-left: 320px";
-      document.getElementById("stock").appendChild (clone); //je fou le clone dans la div #stock
-      document.nameclone.cloneArtiste.value = copy;//Je colle dans la value de Artiste qui est dans #Artiste + 1 (essaie)
-      document.nameclone.cloneTitre.value = copy2; // pareil mais dans Titre
-      i++;
-      return (idclone);
-    }
-
-  function Supprimer (){
-    var element = document.getElementById(idclone); //censé récupérer l'id de l'élément a supprimer
-    element.parentNode.removeChild(element); //censé supprimer l'élément par rapport a son parent
-  }
-*/

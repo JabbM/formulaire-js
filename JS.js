cad02842096e2ceadd2e4b23790a4bd5afb472ea
JabbM <33683864+JabbM@users.noreply.github.com>
2017-12-11 00:11:15 +0100

    Add files via upload

--- JS.js
+++ JS.js
@@ -43,13 +43,6 @@ function enregistrer()
 
 
 }
-  function SelectHoraire()
-        {
-          if (document.getElementById('Horaire').value = "Après diner")
-          {
-              document.getElementById('Heure').style.display = 'block';
-          }
-        }
 
 var id = 1;
         function Ajouter()
@@ -74,26 +67,66 @@ function creerElement()
     Input1.setAttribute('type', 'text');
     Input1.setAttribute('name', 'input1-' + id);
     Input1.setAttribute('id', 'input1-' + id);
+
     var Input2 = document.createElement('input');
     Input2.setAttribute('type', 'text');
     Input2.setAttribute('name', 'input2-' + id);
     Input2.setAttribute('id', 'input2-' + id);
+
     var Select1 = document.createElement('select');
     Select1.setAttribute('id', 'horaire' + id)
     Select1.setAttribute('onchange', 'SelectHoraire()');
+
     var Option1 = document.createElement('option');
     Option1.setAttribute('value', 'Apéro');
-    Option1.setAttribute('value', 'Repas');
-    Option1.setAttribute('value', 'Dessert');
-    Option1.setAttribute('value', 'Après diner');
-    var Select2 = document.createElement('select');
-    Select2.setAttribute('id', 'horaire' + id)
-    Select2.setAttribute('onchange', 'SelectHoraire()');
+    var Opt1 = document.createTextNode('Apéro');
+
     var Option2 = document.createElement('option');
-    Option2.setAttribute('value','22h00');
-    Option2.setAttribute('value', '23h00');
-    Option2.setAttribute('value', '00h00');
-    Option2.setAttribute('value', '01h00');
+    Option2.setAttribute('value', 'Repas');
+    var Opt2 = document.createTextNode('Repas');
+
+    var Option3 = document.createElement('option');
+    Option3.setAttribute('value', 'Dessert');
+    var Opt3 = document.createTextNode('Dessert');
+
+    var Option4 = document.createElement('option');
+    Option4.setAttribute('value', 'Après diner');
+    var Opt4 = document.createTextNode('Après diner');
+
+    var Select2 = document.createElement('select');
+    Select2.setAttribute('id', 'heure' + id)
+
+    var Optionn1 = document.createElement('option');
+    Optionn1.setAttribute('value','22h00');
+    var Optt1 = document.createTextNode('22h00');
+
+    var Optionn2 = document.createElement('option');
+    Optionn2.setAttribute('value', '23h00');
+    var Optt2 = document.createTextNode('23h00');
+
+    var Optionn3 = document.createElement('option');
+    Optionn3.setAttribute('value', '00h00');
+    var Optt3 = document.createTextNode('00h00');
+
+    var Optionn4 = document.createElement('option');
+    Optionn4.setAttribute('value', '01h00');
+    var Optt4 = document.createTextNode('01h00');
+
+    var Optionn5 = document.createElement('option');
+    Optionn5.setAttribute('value', '02h00');
+    var Optt5 = document.createTextNode('02h00');
+
+    var Optionn6 = document.createElement('option');
+    Optionn6.setAttribute('value', '03h00');
+    var Optt6 = document.createTextNode('03h00');
+
+    var Optionn7 = document.createElement('option');
+    Optionn7.setAttribute('value', '04h00');
+    var Optt7 = document.createTextNode('04h00');
+
+    var Optionn8 = document.createElement('option');
+    Optionn8.setAttribute('value', '05h00');
+    var Optt8 = document.createTextNode('05h00');
 
     Conteneur.appendChild(ContainerSong);
     Select1.appendChild(Option1);
@@ -101,10 +134,40 @@ function creerElement()
     ContainerSong.appendChild(Input1);
     ContainerSong.appendChild(Input2);
     Select1.appendChild(Option1);
-    Select2.appendChild(Option2);
+    Select1.appendChild(Option2);
+    Select1.appendChild(Option3);
+    Select1.appendChild(Option4);
+    Option1.appendChild(Opt1);
+    Option2.appendChild(Opt2);
+    Option3.appendChild(Opt3);
+    Option4.appendChild(Opt4);
+    Select2.appendChild(Optionn1);
+    Select2.appendChild(Optionn2);
+    Select2.appendChild(Optionn3);
+    Select2.appendChild(Optionn4);
+    Select2.appendChild(Optionn5);
+    Select2.appendChild(Optionn6);
+    Select2.appendChild(Optionn7);
+    Select2.appendChild(Optionn8);
+    Optionn1.appendChild(Optt1);
+    Optionn2.appendChild(Optt2);
+    Optionn3.appendChild(Optt3);
+    Optionn4.appendChild(Optt4);
+    Optionn5.appendChild(Optt5);
+    Optionn6.appendChild(Optt6);
+    Optionn7.appendChild(Optt7);
+    Optionn8.appendChild(Optt8);
     ContainerSong.appendChild(Select1);
     ContainerSong.appendChild(Select2);
 }
+
+function SelectHoraire()
+      {
+        if (document.getElementById('Horaire' + id).value = "Après diner")
+        {
+            document.getElementById('Heure'+ id).style.display = 'block';
+        }
+      }
 
 function Supprimer(){
 
